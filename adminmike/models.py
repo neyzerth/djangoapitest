766b169d9559b34f0c255801aa99d8bad5699cbb
neyzer <neyzertoledo@gmail.com>
2026-06-17 20:43:37 -0700

    more models

--- adminmike/models.py
+++ adminmike/models.py
@@ -11,34 +11,42 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     hired_at = models.DateTimeField(auto_now_add=True)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
+    subjects = models.ManyToManyField(Subject, blank=True)
 
     def __str__(self):
         return self.name
 
+
+class Career(models.Model):
+    name = models.CharField(max_length=100)
+    semesters = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     enrolled_at = models.DateTimeField(auto_now_add=True)
+    career = models.ForeignKey(Career, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-
-# class Course(models.Model):
-#     name = models.CharField(max_length=200)
-#     code = models.CharField(max_length=20, unique=True)
-#     description = models.TextField(blank=True)
-#     students = models.ManyToManyField(Student, related_name='courses', blank=True)
-#     start_date = models.DateField()
-#     end_date = models.DateField()
-
-#     def __str__(self):
-#         return f"{self.name} ({self.code})"
 
